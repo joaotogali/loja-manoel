1242def16ff3207f3f5ed2195adffc68796c6a06
João Gabriel Mattos Otogali <jao.otogali@gmail.com>
2025-05-30 13:22:31 -0300

    Create models and configure EF Core DbContext. #5

---
+++ LojadoManoelAPI/LojadoManoelAPI/Models/Pedido.cs
@@ -0,0 +1,8 @@
+﻿namespace LojadoManoelAPI.Models
+{
+    public class Pedido
+    {
+        public int PedidoId { get; set; }
+        public List<Produto> Produtos { get; set; }
+    }
+}
---
+++ LojadoManoelAPI/LojadoManoelAPI/Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace LojadoManoelAPI.Models
+{
+    public class Produto
+    {
+        public string ProdutoId {  get; set; }
+        public Dimensoes Dimensoes { get; set; }
+        public int PedidoId { get; set; }
+        public Pedido Pedido { get; set; }
+    }
+}
